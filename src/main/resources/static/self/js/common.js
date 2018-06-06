29d982a9d83bffbae23abe5d263501f5e6571ac9
duanfeiyang <duanfeiyang@bitnei.com>
2018-06-06 14:23:25 +0800

    格式化代码
     - [modify] 格式化代码

--- src/main/resources/static/self/js/common.js
+++ src/main/resources/static/self/js/common.js
@@ -1,15 +1,14 @@
+
 /**
  * 公共常用JS
  */
-
-
-$.fn.serializeObject = function() {
+$.fn.serializeObject = function () {
     var o = {};
     var a = this.serializeArray();
-    $.each(a, function() {
+    $.each(a, function () {
         if (o[this.name]) {
             if (!o[this.name].push) {
-                o[this.name] = [ o[this.name] ];
+                o[this.name] = [o[this.name]];
             }
             o[this.name].push(this.value || '');
         } else {
@@ -22,27 +21,23 @@ $.fn.serializeObject = function() {
 /**
  * 弹出窗口
  */
-function openModuleWin(winid,title,url,width,height) {
+function openModuleWin(winid, title, url, width, height) {
     my_window(winid, title, url, width, height);
 }
-
-
 
 /**
  * 新增窗口
  */
-function openAddDataWin(winid,title,url,width,height,gridId) {
-
+function openAddDataWin(winid, title, url, width, height, gridId) {
     openModuleWin(winid, url, title, width, height);
 }
 
 /**
  * 查看窗口
  */
-function openViewDataWin(winid,title,url,width,height,gridId,id) {
-
+function openViewDataWin(winid, title, url, width, height, gridId, id) {
     if ('' == id || id == undefined) {
-        var row = $("#"+gridId).datagrid("getSelections");
+        var row = $("#" + gridId).datagrid("getSelections");
         if (row != null && row.length == 1) {
             id = row[0].id;
         } else {
@@ -50,7 +45,7 @@ function openViewDataWin(winid,title,url,width,height,gridId,id) {
             return;
         }
     }
-    url += ('?id='+id);
+    url += ('?id=' + id);
     console.log(url);
     openModuleWin(winid, url, title, width, height);
 }
@@ -58,10 +53,9 @@ function openViewDataWin(winid,title,url,width,height,gridId,id) {
 /**
  * 编辑窗口
  */
-function openUpdateDataWin(winid,title,url,width,height,gridId) {
-
+function openUpdateDataWin(winid, title, url, width, height, gridId) {
     if ('' == id || id == undefined) {
-        var row = $("#"+gridId).datagrid("getSelections");
+        var row = $("#" + gridId).datagrid("getSelections");
         if (row != null && row.length == 1) {
             id = row[0].id;
         } else {
@@ -69,40 +63,37 @@ function openUpdateDataWin(winid,title,url,width,height,gridId) {
             return;
         }
     }
-
     openModuleWin(id, url, title, width, height);
 }
 
 /**
  * 删除记录
  */
-function delRecord(title,url,gridId) {
-    var rows = $("#"+gridId).datagrid("getSelections");
+function delRecord(title, url, gridId) {
+    var rows = $("#" + gridId).datagrid("getSelections");
     if (rows != null && rows.length != 0) {
         var ids = "";
-        for(var i=0;i<rows.length;i++){
+        for (var i = 0; i < rows.length; i++) {
             ids += rows[i].id;
-            if(i!=(rows.length-1))
+            if (i != (rows.length - 1))
                 ids += ","
         }
-
-        $.messager.confirm('删除提示', '您确定要删除所选'+title+"?", function(r){
-            if(r){
+        $.messager.confirm('删除提示', '您确定要删除所选' + title + "?", function (r) {
+            if (r) {
                 $.ajax({
-                    url:url,
-                    type:"post",
-                    data:{ids:ids},
-                    dataType:'json',
-                    success:function(data){
-                        $.messager.alert("提示",data.message,'info',function(){
-                            $("#"+gridId).datagrid("reload");
+                    url: url,
+                    type: "post",
+                    data: {ids: ids},
+                    dataType: 'json',
+                    success: function (data) {
+                        $.messager.alert("提示", data.message, 'info', function () {
+                            $("#" + gridId).datagrid("reload");
                         });
                     }
                 });
             }
         });
-    }
-    else {
+    } else {
         alert("请选择记录！");
         return;
     }
@@ -113,26 +104,23 @@ function delRecord(title,url,gridId) {
  * @param formId
  * @param gridId
  */
-function searchDatagrid(formId,gridId){
-
-    var searchParames = $('#'+formId).serializeObject();
-    $("#"+gridId).datagrid("load",searchParames);
+function searchDatagrid(formId, gridId) {
+    var searchParames = $('#' + formId).serializeObject();
+    $("#" + gridId).datagrid("load", searchParames);
 }
-
 
 /**
  * 重置
  * @param formId
  * @param gridId
  */
-function resetDatagrid(formId,gridId){
+function resetDatagrid(formId, gridId) {
     document.getElementById(formId).reset();
-    $("#"+formId+" .textbox-value").val("");
-    $("#"+formId+" .select2").select2().select2("val","");
-    $("#"+formId).form("reset");
-    $("#"+formId+" input[type='hidden']").val("");
-
-    searchDatagrid(formId,gridId);
+    $("#" + formId + " .textbox-value").val("");
+    $("#" + formId + " .select2").select2().select2("val", "");
+    $("#" + formId).form("reset");
+    $("#" + formId + " input[type='hidden']").val("");
+    searchDatagrid(formId, gridId);
 }
 
 /**
@@ -141,36 +129,29 @@ function resetDatagrid(formId,gridId){
  * @param formId
  * @param gridId
  */
-function exportDatagrid(url,formId,gridId){
-
-    if(formId==undefined || formId==null){
+function exportDatagrid(url, formId, gridId) {
+    if (formId == undefined || formId == null) {
         formId = "form_search";
     }
     var myUrl = url;
-    var searchParames = $('#'+formId).serializeObject();
-
-
-    var rows = $("#"+gridId).datagrid("getSelections");
+    var searchParames = $('#' + formId).serializeObject();
+    var rows = $("#" + gridId).datagrid("getSelections");
     if (rows != null && rows.length != 0) {
         var ids = "";
-        for(var i=0;i<rows.length;i++){
+        for (var i = 0; i < rows.length; i++) {
             ids += rows[i].id;
-            if(i!=(rows.length-1))
+            if (i != (rows.length - 1))
                 ids += ","
         }
-
     }
     searchParames['query.ids'] = ids;
-
-    myUrl += '?exportId='+new Date().getTime();
-    for(var key in searchParames){
+    myUrl += '?exportId=' + new Date().getTime();
+    for (var key in searchParames) {
         var value = searchParames[key];
-        if(value!=""){
-            myUrl += ("&"+key+"="+value);
+        if (value != "") {
+            myUrl += ("&" + key + "=" + value);
         }
     }
-
-
-    window.open(myUrl,'_blank');
+    window.open(myUrl, '_blank');
 }
 
