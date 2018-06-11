51c53c7db8a107e751feeb7b10e818e11c2be19b
duanfeiyang <duanfeiyang@bitnei.com>
2018-06-11 09:07:39 +0800

    参数加入用户信息
     - [modify] 参数加入用户信息

--- src/main/java/com/bitnei/cloud/common/PublicDealUtil.java
+++ src/main/java/com/bitnei/cloud/common/PublicDealUtil.java
@@ -1,12 +1,11 @@
 package com.bitnei.cloud.common;
 
+import com.bitnei.cloud.common.util.ServletUtil;
 import com.bitnei.commons.bean.WebUser;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 公共处理类
@@ -17,6 +16,7 @@ public class PublicDealUtil {
     /**
      * TODO 未完成待陈鹏完善用户session信息
      * 处理参数map，加入用户信息参数
+     *
      * @param param
      * @param user
      * @return
@@ -62,5 +62,50 @@ public class PublicDealUtil {
             }
         }
         return rlist;
+    }
+
+    /**
+     * 判断、取值
+     *
+     * @param obj
+     * @return
+     */
+    public static String getMapValueString(Object obj) {
+        if (null != obj) {
+            return obj.toString();
+        }
+        return "";
+    }
+
+    /**
+     * 获取过去第几天的日期(- 操作) 或者 未来 第几天的日期( + 操作)
+     *
+     * @param past
+     * @return
+     */
+    public static String getPastDate(int past) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - past);
+        Date today = calendar.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String result = format.format(today);
+        return result;
+    }
+
+    /**
+     * 给查询参数加入当前用户信息
+     *
+     * @param param
+     * @return
+     */
+    public static Map<String, Object> bulidUserForParams(Map<String, Object> param) {
+        if (null != param) {
+            WebUser user = ServletUtil.getUser();
+            Map<String, Object> map = user.getExtData();
+            param.put("isLeader", map.get("roleType"));
+            param.put("areaPath", map.get("areaPath"));
+            param.put("userUnitPath", map.get("unitPath"));
+        }
+        return param;
     }
 }
